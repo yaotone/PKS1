f9e5bd9cf7400f28d3245570d1d30bac2eb172e7
yaotone <danili4in@gmail.com>
2024-06-13 13:15:39 +0300

    Выполненно

---
+++ PKS1/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PKS1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ PKS1/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+let enterLink = document.getElementById('link')
+enterLink.addEventListener('click', ()=>{
+    document.getElementById('reg').classList.add('hide')
+    document.getElementById('signin').classList.remove('hide')
+})
+
+let regLink = document.getElementById('reg_link')
+regLink.addEventListener('click', ()=>{
+    document.getElementById('reg').classList.remove('hide')
+    document.getElementById('signin').classList.add('hide')
+})
